cae54a1d9f47f4e0f1013b09c2458280c2ff4794
Jorge Moreno <52753718+jopemoma@users.noreply.github.com>
2020-07-12 20:42:34 +0200

    setup to start the project

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from '../../node_modules/@types/react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
