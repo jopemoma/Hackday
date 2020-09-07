2560e0d8b39ca08409e22a8d9257f2bd501730ab
Jorge Moreno <jpmmpj@gmail.com>
2020-09-07 19:46:09 +0200

    ready to deploy on heroku

--- server/app.js
+++ server/app.js
@@ -8,9 +8,17 @@ require('dotenv').config();
 
 const moviedbKey = process.env.MOVIEDBKEY;
 const omdbKey = process.env.OMDBKEY;
-const port = 5000;
+const port = process.env.PORT || 5000;
+const path = require('path');
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.get('/random-movie', async (req, res) => {
   try {
---
+++ server/build/precache-manifest.84778644ff19e8a1f3cd61da744fd94f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac40957734420904d4289fda1e267e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9abda89f1d3adbfea1f9",
+    "url": "/static/css/main.1b1f79d5.chunk.css"
+  },
+  {
+    "revision": "33c6fafe870ef9256e95",
+    "url": "/static/js/2.f4151e37.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f4151e37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9abda89f1d3adbfea1f9",
+    "url": "/static/js/main.7a888ab4.chunk.js"
+  },
+  {
+    "revision": "620b8b10a92405982d7d",
+    "url": "/static/js/runtime-main.8a4eeed6.js"
+  }
+]);
